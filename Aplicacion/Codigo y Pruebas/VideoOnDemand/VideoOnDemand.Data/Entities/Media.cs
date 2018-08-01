2efe8c52872ef2c2945e750d805ce73dea52db64
Jesus Miranda <jesusmiranda0304@gmail.com>
2018-08-01 11:45:14 -0500

    Clase Agregada
    Clase Media Actualizada
    clase EstatusMedia Creada

--- Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Entities/Media.cs
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Entities/Media.cs
@@ -8,13 +8,15 @@ namespace VideoOnDemand.Entities
 {
     public class Media
     {
-        public int MediaId { get; set; }
+        public int? MediaId { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public int DuracionMin { get; set; }
-        public DateTime FechaRegistro { get; set; }
-        public DateTime FechaLanzamiento { get; set; }
-
-
+        public int? DuracionMin { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public DateTime? FechaLanzamiento { get; set; }
+        public bool Activo { get; set; }
+        public EEstatusMedia? Estatus { get; set; }
+        public ICollection<Genero> Generos { get; set; }
+        //public ICollection<Persona> Actores { get; set; }
     }
 }

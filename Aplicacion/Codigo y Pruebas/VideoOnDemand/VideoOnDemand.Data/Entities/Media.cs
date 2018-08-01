7dd8edf2f4eff08e32ef8b090b4e4436634b27c7
VictorMedinaGomez <VictorMedinaGomez@users.noreply.github.com>
2018-08-01 12:10:45 -0500

    llll

--- Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Entities/Media.cs
+++ Aplicacion/Codigo y Pruebas/VideoOnDemand/VideoOnDemand.Data/Entities/Media.cs
@@ -11,18 +11,13 @@ namespace VideoOnDemand.Entities
         public int? MediaId { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-<<<<<<< HEAD
-        public int DuracionMin { get; set; }
-        public DateTime FechaRegistro { get; set; }
-        public DateTime FechaLanzamiento { get; set; }
-=======
         public int? DuracionMin { get; set; }
         public DateTime? FechaRegistro { get; set; }
         public DateTime? FechaLanzamiento { get; set; }
         public bool Activo { get; set; }
-        public EEstatusMedia? Estatus { get; set; }
+
         public ICollection<Genero> Generos { get; set; }
         //public ICollection<Persona> Actores { get; set; }
->>>>>>> c117093dd54d48d52c22161bfcdcb56fd5d33493
+
     }
 }
